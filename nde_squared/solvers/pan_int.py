26defacc2d817f778e6e7b5bbe8c4fd205ddcf46
Gamma-Pan <yiorgospanag@gmail.com>
2024-02-23 16:04:47 +0200

    starting pan integration routine

--- nde_squared/solvers/pan_int.py
+++ nde_squared/solvers/pan_int.py
@@ -1,6 +1,20 @@
+from typing import Iterable
+
 import torch
 from torch import tensor, Tensor
 from ..optim import newton
 
-def pan_int(f, y_init, grid=None, etol=1e-3, Phi=None, DPhi=None)->Tensor:
 
+def pan_int(f, y_init, t_lims: Iterable, step:torch.float, etol=1e-3) -> tuple[Tensor,Tensor]:
+    def Phi_s(t: Tensor) -> Tensor:
+        """
+        This function returns a matrix Phi that when multiplied with the  coefficients matrix
+        calculates the polynomial approximation of y(t) at the defined grid points
+
+        :param t_lims: the initial and terminal time of integration
+        :param y_init: the value of y at initial time
+        :return: a tuple of the coefficients matrix and the Phi matrix
+        """
+
+
+    def Phi_c(t: Tensor) -> Tensor:
