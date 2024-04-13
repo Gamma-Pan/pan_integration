17393cd9f71a041712389bc865735995c12b6df3
Gamma-Pan <yiorgospanag@gmail.com>
2024-04-13 21:45:18 +0300

    testing Phi at custom points

--- pan_int_batch_test.py
+++ pan_int_batch_test.py
@@ -40,15 +40,8 @@ if __name__ == "__main__":
     y_init = torch.rand(batches, dims)
     f_init = f(0, y_init)
 
-    t_lims = [0.0, 5.0]
-    t_span = torch.linspace(*t_lims, 10)
 
-    plotter = VfPlotter(f)
-    plotter.solve_ivp(t_span, y_init, set_lims=True,plot_kwargs=dict(alpha=0.3))
-
-    t_eval, y_tsit = odeint(f, y_init, t_span, "tsit5")
-
-    num_coeff_per_dim = 8
+    # t_eval, y_tsit = odeint(f, y_init, t_span, "tsit5")
 
     def callback(B_vec):
         Phi, DPhi = _cheb_phis(
@@ -72,17 +65,22 @@ if __name__ == "__main__":
         plotter.approx(approx, t_lims[0], Dapprox=Dapprox)
         wait()
 
-
     # solver = LSZero(var_num_coeff_per_dim, var_num_points)
     # _, approx_ms = odeint_mshooting(f, var_y_init, var_t_span, solver, torch.empty(0))
 
-    B = lst_sq_solver(f,t_lims, y_init,num_coeff_per_dim,num_coeff_per_dim, f_init=f_init)
+    t_lims = [0.0, 5.0]
+    num_points = 10
+    num_coeff_per_dim = 8
 
-    t_span = -torch.cos(torch.pi * (torch.arange(10) / 9))
+    B = lst_sq_solver(f,t_lims, y_init,num_coeff_per_dim,num_points, f_init=f_init)
 
+    t_span = -torch.cos(torch.pi * (torch.arange(num_points+1) / num_points))
     Phi, _ = _cheb_phis(t_span, num_coeff_per_dim)
-
     approx = (Phi @ B).transpose(0, 1)
 
+
+    plotter = VfPlotter(f)
+    plotter.solve_ivp(torch.linspace(*t_lims, steps=100), y_init, set_lims=True,plot_kwargs=dict(alpha=0.3))
+    wait()
     plotter.approx(approx, t_lims[0], color="orange")
     wait()
