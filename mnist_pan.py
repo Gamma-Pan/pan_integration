93785f69829a48f1d82b666638118793baa23d80
Gamma-Pan <yiorgospanag@gmail.com>
2024-05-25 14:43:52 +0300

    testing all on convolutional

--- mnist_pan.py
+++ mnist_pan.py
@@ -15,6 +15,8 @@ from pan_integration.utils.lightning import LitOdeClassifier
 
 import wandb
 
+from copy import copy
+
 torch.manual_seed(42)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -25,13 +27,12 @@ import multiprocessing as mp
 NUM_WORKERS = mp.cpu_count()
 CHANNELS = 32
 NUM_GROUPS = 4
-WANDB_LOG = True
+WANDB_LOG = False
 
 embedding = nn.Sequential(
-    # nn.Conv2d(1, CHANNELS, 3, 2, padding=1),
-    # nn.GroupNorm(NUM_GROUPS, CHANNELS),
-    # nn.Tanh(),
-    nn.Flatten()
+    nn.Conv2d(1, CHANNELS, 3, 2, padding=1),
+    nn.GroupNorm(NUM_GROUPS, CHANNELS),
+    nn.Tanh(),
 ).to(device)
 
 
@@ -39,32 +40,26 @@ class VF(nn.Module):
     def __init__(self):
         super().__init__()
         self.nfe = 0
-        # self.norm1 = nn.GroupNorm(NUM_GROUPS, CHANNELS)
-        # self.conv1 = nn.Conv2d(CHANNELS, CHANNELS, 3, 1, padding=1)
-        # self.norm2 = nn.GroupNorm(NUM_GROUPS, CHANNELS)
-        # self.conv2 = nn.Conv2d(CHANNELS, CHANNELS, 3, 1, padding=1)
-        self.lin1 = nn.Linear(28**2, 28**2)
-        self.lin2 = nn.Linear(28**2, 28**2)
-        self.lin3 = nn.Linear(28**2, 28**2)
-        self.nl_fn = nn.ReLU()
+        self.norm1 = nn.GroupNorm(NUM_GROUPS, CHANNELS)
+        self.conv1 = nn.Conv2d(CHANNELS, CHANNELS, 3, 1, padding=1)
+        self.norm2 = nn.GroupNorm(NUM_GROUPS, CHANNELS)
+        self.conv2 = nn.Conv2d(CHANNELS, CHANNELS, 3, 1, padding=1)
+        self.norm3 = nn.GroupNorm(NUM_GROUPS, CHANNELS)
 
     def forward(self, t, x, *args, **kwargs):
         self.nfe += 1
-        # x = self.nl_fn(self.norm1(self.conv1(x)))
-        # x = self.nl_fn(self.norm2(self.conv2(x)))
-        x = self.nl_fn(self.lin1(x))
-        x = self.nl_fn(self.lin2(x))
-        x = self.nl_fn(self.lin3(x))
+        x = F.relu(self.norm1(x))
+        x = F.relu(self.norm2(self.conv1(x)))
+        x = self.norm3(self.conv2(x))
         return x
 
 
 classifier = nn.Sequential(
-    # nn.Dropout(0.01),
-    # nn.Conv2d(CHANNELS, 1, 3, 1),
-    # nn.ReLU(),
-    # nn.Flatten(),
-    # nn.Linear(144, 10),
-    nn.Linear(28**2, 10),
+    nn.Dropout(0.01),
+    nn.Conv2d(CHANNELS, 1, 3, 1),
+    nn.ReLU(),
+    nn.Flatten(),
+    nn.Linear(144, 10),
 )
 
 
@@ -85,45 +80,19 @@ def train_mnist_ode(t_span, ode_model, epochs=10, test=False, logger=()):
         trainer.test(learner, datamodule=dmodule)
 
 
-def train_all_pan(configs=None, sensitivity='autograd', epochs=50):
-    t_span = torch.linspace(0,1,10).to(device)
+def train_all_pan(configs=None, sensitivity="autograd", epochs=50):
+    t_span = torch.linspace(0, 1, 2).to(device)
 
     if configs is None:
         configs = (
+            {"num_coeff_per_dim": 8, "num_points": 8, "delta": 1e-3, "max_iters": 20},
             {"num_coeff_per_dim": 16, "num_points": 16, "delta": 1e-3, "max_iters": 20},
             {"num_coeff_per_dim": 32, "num_points": 32, "delta": 1e-3, "max_iters": 20},
             {"num_coeff_per_dim": 64, "num_points": 64, "delta": 1e-3, "max_iters": 20},
+            {"num_coeff_per_dim": 8, "num_points": 8, "delta": 1e-3, "max_iters": 20},
             {"num_coeff_per_dim": 16, "num_points": 16, "delta": 1e-2, "max_iters": 20},
             {"num_coeff_per_dim": 32, "num_points": 32, "delta": 1e-2, "max_iters": 20},
             {"num_coeff_per_dim": 64, "num_points": 64, "delta": 1e-2, "max_iters": 20},
-        )
-
-    for config in configs:
-        vf = VF().to(device)
-        logger=()
-        if WANDB_LOG:
-            logger = WandbLogger(
-                project="pan_integration",
-                name=f"pan_{config.num_coeff_per_dim}_{config.num_points}",
-                log_model=False,
-            )
-            logger.experiment.config.update(config)
-            logger.experiment.config.update({"type": "pan", "sensitivity": sensitivity})
-
-        model = PanODE(vf, t_span, solver=config, solver_adjoint=config, sensitivity=sensitivity).to(device)
-        train_mnist_ode(t_span, model, epochs=epochs, test=True, logger=logger)
-
-
-def train_all_shooting(configs=None, sensitivity='autograd', epochs=50):
-    t_span = torch.linspace(0,1,10).to(device)
-
-    if configs is None:
-        configs = (
-            {"solver":'dopri5', "atol":1e-3 },
-            {"solver":'tsit5', "atol":1e-3 ,},
-            {"solver":'dopri5', "atol":1e-4 },
-            {"solver":'tsit5', "atol":1e-4 },
-            {"solver":'rk-4',}
         )
 
     for config in configs:
@@ -132,19 +101,60 @@ def train_all_shooting(configs=None, sensitivity='autograd', epochs=50):
         if WANDB_LOG:
             logger = WandbLogger(
                 project="pan_integration",
-                name=f"shooting_f{config.solver}",
+                name=f"pan_{config['num_coeff_per_dim']}_{config['num_points']}",
                 log_model=False,
             )
             logger.experiment.config.update(config)
-            logger.experiment.config.update({"type": "shooting", "sensitivity": sensitivity, 'fixed_step':10})
+            logger.experiment.config.update(
+                {"type": "pan", "sensitivity": sensitivity, "architecture": "CNN"}
+            )
 
-        model = NeuralODE(vf,  **config, sensitivity=sensitivity)
+        model = PanODE(
+            vf, t_span, solver=config, solver_adjoint=config, sensitivity=sensitivity
+        ).to(device)
         train_mnist_ode(t_span, model, epochs=epochs, test=True, logger=logger)
+        if WANDB_LOG:
+            wandb.finish()
+
+
+def train_all_shooting(configs=None, sensitivity="autograd", epochs=50):
+    if configs is None:
+        configs = (
+            {"solver": "dopri5", "atol": 1e-3, "fixed_steps": 2},
+            {"solver": "tsit5", "atol": 1e-3, "fixed_steps": 2},
+            {"solver": "dopri5", "atol": 1e-4, "fixed_steps": 2},
+            {"solver": "tsit5", "atol": 1e-4, "fixed_steps": 2},
+            {"solver": "rk-4", "fixed_steps": 2},
+            {"solver": "rk-4", "fixed_steps": 5},
+            {"solver": "rk-4", "fixed_steps": 10},
+        )
+
+    for config in configs:
+        vf = VF().to(device)
+        logger = ()
+        if WANDB_LOG:
+            logger = WandbLogger(
+                project="pan_integration",
+                name=f"shooting_{config['solver']}",
+                log_model=False,
+            )
+            logger.experiment.config.update(config)
+            logger.experiment.config.update(
+                {"type": "shooting", "sensitivity": sensitivity, "architecture": "CNN"}
+            )
+
+        _config = copy(config)
+        del _config["fixed_steps"]
+
+        model = NeuralODE(vf, **_config, sensitivity=sensitivity)
+        t_span = torch.linspace(0, 1, int(config["fixed_steps"])).to(device)
+        train_mnist_ode(t_span, model, epochs=epochs, test=False, logger=logger)
+        if WANDB_LOG:
+            wandb.finish()
 
 
 if __name__ == "__main__":
-    train_all_pan(epochs=50)
-    train_all_shooting(epochs=50)
-
-
-
+    train_all_pan(epochs=40, sensitivity="autograd")
+    train_all_shooting(epochs=40, sensitivity="autograd")
+    # train_all_shooting(epochs=30, sensitivity="adjoint")
+    # train_all_pan(epochs=30, sensitivity="adjoint")
