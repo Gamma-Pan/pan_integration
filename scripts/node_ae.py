dc182068d59099e6edb68067b9d621ba5bd92a72
Gamma-Pan <yiorgospanag@gmail.com>
2024-11-02 01:13:54 +0200

    linear projection node autoencoder

--- scripts/node_ae.py
+++ scripts/node_ae.py
@@ -149,19 +149,111 @@ class SSAutoEncoder(LightningModule):
         self.log("test_loss", loss, on_step=False, on_epoch=True)
 
 
+class SubSpaceNODEProj(LightningModule):
+    def __init__(self, input_size:tuple= (1, 28, 28), latent_dim:int=2, channels:int=16):
+        super().__init__()
+
+        self.augmenter = ILAugmenter(out_ch=channels)
+        self.output_layer = nn.Sequential( nn.Conv2d(channels, input_size[0], 1, 1), nn.Sigmoid())
+
+        vf = VF()
+        self.encoder = NODEWrapper(vf)
+
+        self.ss_basis = nn.Parameter(torch.randn( prod(input_size[1:])*channels ,latent_dim), requires_grad=True)
+
+        self.learning_rate = 1e-3
+
+
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(
+            self.parameters(), lr=self.learning_rate, weight_decay=5 * 1e-4
+        )
+        lr_scheduler_config = {
+            "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                opt, factor=0.5, patience=5, cooldown=3, min_lr=5e-7
+            ),
+            "monitor": "training_loss",
+            "interval": "epoch",
+            "frequency": 1,
+        }
+        out = {"optimizer": opt, "lr_scheduler": lr_scheduler_config}
+        return out
+
+    def _common_step(self, x):
+
+        x = self.augmenter(x)
+        enc_out = self.encoder(x)
+        # ??? how do I project tensor?
+        z = enc_out.view(x.size(0), -1 )
+        encoding =  (self.ss_basis @ ( inv( self.ss_basis.mT @  self.ss_basis) @ (self.ss_basis.mT @ z[...,None]) ))[...,0]
+
+        return x, encoding, z
+
+    def forward(self, x):
+        decoding, encoding, z= self._common_step(x)
+        coordinates =  (pinv(self.ss_basis) @ encoding[...,None])[...,0]
+
+        return decoding, coordinates
+
+    def training_step(self, batch, batch_idx):
+        x, label = batch
+        decoding, encoding, z = self._common_step(x)
+
+        # reconstruction_loss = F.mse_loss(decoding, x, reduction='mean')
+        projection_loss = F.mse_loss(encoding, z, reduction='mean')
+
+        # self.log("reconstruction_loss", reconstruction_loss, prog_bar=True)
+        self.log("projection_loss", projection_loss, prog_bar=True)
+
+        loss = projection_loss
+
+        self.log("training_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, label = batch
+        decoding, encoding, z = self._common_step(x)
+
+        # reconstruction_loss = F.mse_loss(decoding, x, reduction='sum')
+        projection_loss = F.mse_loss(encoding, z, reduction='sum')
+
+        loss = projection_loss
+
+        # find the subspace coordinates
+
+        coordinates =  (pinv(self.ss_basis) @ encoding[...,None])[...,0]
+
+        self.log("validation_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        return {"deconding": decoding, "encoding": coordinates}
+
+    def test_step(self, batch, batch_idx):
+        x, label = batch
+        decoding, encoding, z = self._common_step(x)
+
+        reconstruction_loss = F.mse_loss(decoding, x, reduction='sum')
+        projection_loss = F.mse_loss(encoding, z, reduction='sum')
+
+        loss = reconstruction_loss + projection_loss
+
+        self.log("test_loss", loss, on_step=False, on_epoch=True)
+
+
 if __name__ == '__main__':
 
     num_workers = multiprocessing.cpu_count()
 
-    mnist_module = MNISTDataModule(num_workers=num_workers, batch_size=16)
+    mnist_module = MNISTDataModule(num_workers=num_workers, batch_size=128)
     wandb_logger = WandbLogger(project="ode_encoder")
     callbacks = [
-        DigitsReconstruction(num_digits=24),
+        # DigitsReconstruction(num_digits=24),
         LearningRateMonitor(logging_interval="epoch"),
         LatentSpace()
     ]
 
-    model = SSAutoEncoder(input_size=(1, 32, 32), latent_dim=2, channels=16)
+    # model = SSAutoEncoder(input_size=(1, 32, 32), latent_dim=2, channels=16)
+    model = SubSpaceNODEProj(input_size=(1,32,32), latent_dim=2, channels=16)
 
     trainer = Trainer(
         fast_dev_run=False,
